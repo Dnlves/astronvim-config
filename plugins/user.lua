38319e1f132e0cfe98194f5226f4ccafeca11608
Diogo Alves <dnalves52@gmail.com>
2023-09-01 01:40:35 -0300

    theme and options

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,17 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  -- {
+  --   "sainnhe/sonokai",
+  --   name = "sonokai",
+  --   config = function()
+  --     require("sonokai").setup {}
+  --   end,
+  -- },
+    {
+      "sainnhe/sonokai",
+      init = function() -- init function runs before the plugin is loaded
+        vim.g.sonokai_style = "andromeda"
+      end,
+    },
 }
