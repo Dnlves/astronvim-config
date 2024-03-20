930c2e74438d02cfb8551d69118806c4e901096b
Diogo Alves <dnalves52@gmail.com>
2024-03-20 00:12:41 -0300

    mappins and markdown plugin

--- mappings.lua
+++ mappings.lua
@@ -30,6 +30,8 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    -- select all
+    ["<C-a>"] = { "ggVG", desc = "Select All Text" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
